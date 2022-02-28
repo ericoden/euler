a9d24fa7193ee3185564885921226bc7be06cd79
ericoden <ericjoden94@gmail.com>
2022-02-28 14:19:58 -0500

    make cosmetic change

--- problem_9.cpp
+++ problem_9.cpp
@@ -16,7 +16,7 @@ int main() {
     int result = 0;
     while (result == 0) {
         b = 1;
-        while (result == 0 & b <= 1000) {
+        while ((result == 0) & (b <= 1000)) {
             c = 1000 - a - b;
             if (std::pow(a, 2) + std::pow(b, 2) == std::pow(c, 2)) {
                 result = a * b * c;
@@ -25,7 +25,9 @@ int main() {
         }
         a++;
     }
-    std::cout << "Answer: " << a - 1 << " * " << b - 1 << " * " << c;
+    a--;
+    b--;
+    std::cout << "Answer: " << a << " * " << b << " * " << c;
     std::cout << " = " << result << std::endl;
     return 0;
 }
